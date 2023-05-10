0e332974260624770fc2181fef28d835d643a4a7
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-05-10 22:51:59 +0300

    assignment

---
+++ assignment.js
@@ -0,0 +1,35 @@
+
+// Write an asynchronous function that accepts a message string 
+// and a delay time in milliseconds.
+//  The function should log the message to the console after the specified delay time.
+
+let felicitation = () =>{
+    console.log('Congratulations you did it')
+};
+felicitation();
+setTimeout(felicitation,3000);
+
+// You have an array of user IDs and a function getUserData(id) that returns
+//  a Promise with user data when given a user ID. 
+//  Write an asynchronous function that fetches and logs the data 
+//  for each user ID one by one, in sequence.
+
+
+
+
+
+
+  
+
+
+ 
+  
+  
+  
+  
+  
+  
+  
+
+
+
